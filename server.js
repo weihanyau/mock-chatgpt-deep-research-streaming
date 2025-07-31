af0cf23ae35372fc6ca42e7d85c418b35451fd12
weihanyau <weihanyau@gmail.com>
2025-08-01 00:26:48 +0800

    setup web page hosting

--- server.js
+++ server.js
@@ -14,9 +14,15 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('.'));
 
 const kafkaEmitter = new EventEmitter();
 kafkaEmitter.setMaxListeners(0);
+
+// Serve the test HTML page
+app.get('/', (req, res) => {
+  res.sendFile('test-stream.html', { root: __dirname });
+});
 
 // Initialize MongoDB connection
 await connect();
